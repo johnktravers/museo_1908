3de2d3b6e05854843bf7f4fec2e8043fca5f7d09
John Travers <johnk.travers@gmail.com>
2019-09-17 09:32:52 -0600

    Add find_by_id methods

--- lib/curator.rb
+++ lib/curator.rb
@@ -14,4 +14,12 @@ class Curator
     @artists.push(artist)
   end
 
+  def find_artist_by_id(artist_id)
+    @artists.find { |artist| artist.id == artist_id }
+  end
+
+  def find_photograph_by_id(photo_id)
+    @photographs.find { |photo| photo.id == photo_id }
+  end
+
 end
